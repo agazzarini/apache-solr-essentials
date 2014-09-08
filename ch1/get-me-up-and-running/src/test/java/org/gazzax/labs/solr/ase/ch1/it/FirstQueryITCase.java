df6745e340497d0f7c3bbb87dd97b08883ee6dc8
agazzarini <agazzarini@andrea-laptop>
2014-09-08 18:26:24 +0200

    [ issue #1 ] comments and minor refactoring on ch1 test case

--- ch1/get-me-up-and-running/src/test/java/org/gazzax/labs/solr/ase/ch1/it/FirstQueryITCase.java
+++ ch1/get-me-up-and-running/src/test/java/org/gazzax/labs/solr/ase/ch1/it/FirstQueryITCase.java
@@ -31,6 +31,58 @@ import org.junit.Test;
 public class FirstQueryITCase {
 	
 	private SolrServer client;
+
+	/**
+	 * Sample and simple scenario where we index one document and make a query.
+	 * 
+	 * @throws Exception hopefully never otherwise the test fails.
+	 */
+	@Test
+	public void indexAndQuery() throws Exception {
+		// This is the (input) Data Transfer Object between your client and SOLR.
+		final SolrInputDocument input = new SolrInputDocument();
+		
+		// 1. Populates with (at least required) fields
+		input.setField("id", 1);
+		input.setField("title", "Apache SOLR Essentials");
+		input.setField("author", "Andrea Gazzarini");
+		input.setField("isbn", "972-2-5A619-12A-X");
+		
+		// 2. Adds the document
+		client.add(input);
+		
+		// 3. Makes index changes visible
+		client.commit();
+		
+		// 4. Builds a new query object with a "select all" query. 
+		final SolrQuery query = new SolrQuery("*:*");
+		
+		// 5. Executes the query
+		final QueryResponse response = client.query(query);
+		
+		assertEquals(1, response.getResults().getNumFound());
+		
+		// 6. Gets the (output) Data Transfer Object.
+		final SolrDocument output = response.getResults().iterator().next();
+		final String id = (String) output.getFieldValue("id");
+		final String title = (String) output.getFieldValue("title");
+		final String author = (String) output.getFieldValue("author");
+		final String isbn = (String) output.getFieldValue("isbn");
+		
+		// 7.1 In case we are running as a Java application print out the query results.
+		System.out.println("It works! I found the following book: ");
+		System.out.println("--------------------------------------");
+		System.out.println("ID: " + id);
+		System.out.println("Title: " + title);
+		System.out.println("Author: " + author);
+		System.out.println("ISBN: " + isbn);
+		
+		// 7. Otherwise asserts the query results using standard JUnit procedures.
+		assertEquals("1", id);
+		assertEquals("Apache SOLR Essentials", title);
+		assertEquals("Andrea Gazzarini", author);
+		assertEquals("972-2-5A619-12A-X", isbn);
+	}
 	
 	/**
 	 * Entry point for running the example as a Java application.
@@ -52,58 +104,6 @@ public class FirstQueryITCase {
 	@Before
 	public void setUp() {
 		client = new HttpSolrServer("http://127.0.0.1:8983/solr/biblo");
-	}
-	
-	/**
-	 * Sample and simple scenario where we index one document and make a query.
-	 * 
-	 * @throws Exception hopefully never otherwise the test fails.
-	 */
-	@Test
-	public void indexAndQuery() throws Exception {
-		// This is the (input) Data Transfer Object between your client and SOLR.
-		final SolrInputDocument inputDocument = new SolrInputDocument();
-		
-		// 1. Populates with (at least required) fields
-		inputDocument.setField("id", 1);
-		inputDocument.setField("title", "Apache SOLR Essentials");
-		inputDocument.setField("author", "Andrea Gazzarini");
-		inputDocument.setField("isbn", "972-2-5A619-12A-X");
-		
-		// 2. Adds the document
-		client.add(inputDocument);
-		
-		// 3. Makes index changes visible
-		client.commit();
-		
-		// 4. Builds a new query object with a "select all" query. 
-		final SolrQuery query = new SolrQuery("*:*");
-		
-		// 5. Executes the query
-		final QueryResponse response = client.query(query);
-		
-		assertEquals(1, response.getResults().getNumFound());
-		
-		// 6. Gets the (output) Data Transfer Object.
-		final SolrDocument outputDocument = response.getResults().iterator().next();
-		final String id = (String) outputDocument.getFieldValue("id");
-		final String title = (String) outputDocument.getFieldValue("title");
-		final String author = (String) outputDocument.getFieldValue("author");
-		final String isbn = (String) outputDocument.getFieldValue("isbn");
-		
-		// 7.1 In case we are running as a Java application print out the query results.
-		System.out.println("It works! I found the following book: ");
-		System.out.println("--------------------------------------");
-		System.out.println("ID: " + id);
-		System.out.println("Title: " + title);
-		System.out.println("Author: " + author);
-		System.out.println("ISBN: " + isbn);
-		
-		// 7. Otherwise asserts the query results using standard JUnit procedures.
-		assertEquals("1", id);
-		assertEquals("Apache SOLR Essentials", title);
-		assertEquals("Andrea Gazzarini", author);
-		assertEquals("972-2-5A619-12A-X", isbn);
 	}
 	
 	/**
