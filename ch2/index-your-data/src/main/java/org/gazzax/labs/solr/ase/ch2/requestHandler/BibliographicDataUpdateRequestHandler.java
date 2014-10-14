d568f3bd0397fd79f61847c1421628102c6f2ead
agazzarini <agazzarini@agazzarini-K72Jr>
2014-10-14 12:31:41 +0200

    [ issue #2 ] first draft structure of the project for Ch2

---
+++ ch2/index-your-data/src/main/java/org/gazzax/labs/solr/ase/ch2/requestHandler/BibliographicDataUpdateRequestHandler.java
@@ -0,0 +1,7 @@
+package org.gazzax.labs.solr.ase.ch2.requestHandler;
+
+import org.apache.solr.handler.UpdateRequestHandler;
+
+public class BibliographicDataUpdateRequestHandler extends UpdateRequestHandler {
+
+}
---
+++ ch2/index-your-data/src/main/java/org/gazzax/labs/solr/ase/ch2/urp/SplitFieldValueProcessorFactory.java
@@ -0,0 +1,17 @@
+package org.gazzax.labs.solr.ase.ch2.urp;
+
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.apache.solr.update.processor.UpdateRequestProcessorFactory;
+
+public class SplitFieldValueProcessorFactory extends UpdateRequestProcessorFactory {
+
+	@Override
+	public UpdateRequestProcessor getInstance(SolrQueryRequest arg0,
+			SolrQueryResponse arg1, UpdateRequestProcessor arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
