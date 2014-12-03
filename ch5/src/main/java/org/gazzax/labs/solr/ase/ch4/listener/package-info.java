67120eee31c63e1b1d36111a283236c4ce47c783
agazzarini <agazzarini@andrea-desktop>
2014-12-03 16:04:32 +0100

    [ issue #5 ] Initial sample project layout

---
+++ ch5/src/main/java/org/gazzax/labs/solr/ase/ch4/listener/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains a {@link org.apache.solr.core.SolrEventListener} that 
+ * automatically preload sample data on Solr startup.
+ */
+package org.gazzax.labs.solr.ase.ch4.listener;
